98f2c00e6ad2d4d4f01d1bed7cf7e89cfa646d6d
Rebecca <rebecca@psafe.com>
2018-04-02 22:18:36 -0300

    Fim: 2.5 Gerenciando a localização na aplicação

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import ListContacts from './listContacts'
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
-  
+
   state = {
     contacts: []
   }
-  
+
   componentDidMount() {
     ContactsAPI.getAll().then((contacts) => {
       this.setState({ contacts })
@@ -22,14 +24,34 @@ class App extends Component {
 
     ContactsAPI.remove(contact)
 
-  } 
+  }
+
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
 
   render() {
     return (
       <div>
-        <ListContacts
-          onDeleteContact={this.removeContact}
-          contacts={this.state.contacts} />
+        <Route exact path='/' render={() => (
+          <ListContacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+           />
+        )}/>
+
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )} />
       </div>
     )
   }
